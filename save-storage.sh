d17900499f39170431d1f75b3dca8ac75564e338
Parmjot Singh <parmjotsinghrobot@gmail.com>
2022-11-24 12:12:24 +1300

    remove unneeded packages and directorys
    lmao I can't even spell; last commit message

--- save-storage.sh
+++ save-storage.sh
@@ -2,3 +2,6 @@ sudo rm -rf /usr/share/dotnet
 sudo rm -rf /opt/ghc
 sudo rm -rf "/usr/local/share/boost"
 sudo rm -rf "$AGENT_TOOLSDIRECTORY"
+sudo rm -rf /home/linuxbrew
+
+sudo apt remove ruby-full fonts-noto-color-emoji 
